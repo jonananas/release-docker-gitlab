874e4ce2843c57fe3adb4fc5b1e7cf887f2028f9
Jonas Andersson <jonas.andersson@algoryx.se>
2021-03-30 08:54:51 +0200

    Add snapshot and master-branch checks
    - Add pylance to vscode and fix warnings

--- tests/test_semver.py
+++ tests/test_semver.py
@@ -1,6 +1,7 @@
 from pytest import raises
 from semver import SemVer
 
+
 def test_has_major_minor_patch():
     semver = SemVer("0.1.2-SNAPSHOT")
     assert semver.major == 0
@@ -8,16 +9,19 @@ def test_has_major_minor_patch():
     assert semver.patch == 2
     assert semver.extra == "-SNAPSHOT"
 
+
 def test_that_extra_is_none():
     semver = SemVer("0.1.2")
     assert semver.major == 0
     assert semver.minor == 1
     assert semver.patch == 2
-    assert semver.extra == None
+    assert semver.extra is None
+
 
 def test_that_fails_without_major_minor_patch():
     with raises(Exception):
         SemVer("0.1")
+
 
 def test_str():
     assert str(SemVer("0.1.2-SNAPSHOT")) == SemVer("0.1.2-SNAPSHOT").str()
@@ -25,12 +29,15 @@ def test_str():
     assert SemVer("0.1.2").str() == "0.1.2"
     assert SemVer("0.1.2-").str() == "0.1.2-"
 
+
 def test_next_patch_of_snapshot():
     assert SemVer("0.1.2-SNAPSHOT").next_patch() == SemVer("0.1.3-SNAPSHOT")
+
 
 def test_release():
     semver = SemVer("0.1.2-SNAPSHOT")
     assert semver.release().str() == "0.1.2"
+
 
 def test_equality():
     verstr = "0.1.2-SNAPSHOT"
@@ -40,4 +47,4 @@ def test_equality():
     assert semver == same
     assert semver != other
     assert semver != verstr
-    assert semver != None
+    assert semver is not None

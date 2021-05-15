497894bf78ad77d07b8a0c384698f20bed7d2e10
Jonas Andersson <jonananas@gmail.com>
2021-05-15 13:45:49 +0200

    Add package info and directory structure

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="release-docker-gitlab",
+    version="0.0.1-SNAPSHOT",
+    author="Jonas Andersson",
+    author_email="jonananas@gmail.com",
+    description="Release Docker GitLab projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jonananas/release-docker-gitlab",
+    project_urls={
+        "Bug Tracker": "https://github.com/jonananas/release-docker-gitlab/issues"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.9",
+)

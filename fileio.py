b96759ee70eae8c45f714ac10bc60af2b4bb736e
Jonas Andersson <jonas.andersson@algoryx.se>
2021-06-09 10:00:11 +0200

    Use types.List instead of list

--- fileio.py
+++ fileio.py
@@ -1,10 +1,12 @@
+from types import List
+
 
 class FileIO:
-    def readlines(self, filename: str) -> list[str]:
+    def readlines(self, filename: str) -> List[str]:
         with open(filename, "r") as file:
             return file.readlines()
 
-    def writelines(self, filename: str, lines: list[str]):
+    def writelines(self, filename: str, lines: List[str]):
         with open(filename, "w") as file:
             for line in lines:
                 file.write(line)
